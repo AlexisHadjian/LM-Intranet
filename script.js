6e1c0a3caafef3fc159cee62cd97f463509a580d
Alexis Hadjian <alexis@MacBook-Air-de-Alexis.local>
2023-01-09 17:04:17 +0100

    header home user info

--- script.js
+++ script.js
@@ -9,9 +9,25 @@ jQuery(document).ready(function($){
 
 		var id = $(this).attr('id');
 
-		$('#tabs li, #tabs-content .tab').removeClass('active');//Remove all tab class
-		$('#tabs #'+id+', #content__'+id+'').addClass('active');//Add current tab class 
+		$('#tabs li, #content-tabs .tab').removeClass('active');//Remove all tab class
+		$('#tabs #'+id+', #content-'+id+'').addClass('active');//Add current tab class 
 	});
 
+
+	/*--------------------------------------------------------------
+    Home blog slider
+    --------------------------------------------------------------*/
+
+	if ($('#content-tabs__home__actuality__blog').length > 0) {
+        $('#content-tabs__home__actuality__blog').slick({
+            infinite: true,
+            autoplay: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplaySpeed: 2000,
+            centerMode: true,
+        });
+
+    }
 
 });
